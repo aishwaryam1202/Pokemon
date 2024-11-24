2d175a2a6c1f58d2ee9fe35002553e581a394497
Aishwarya Manoharan <74893699+aishwaryam1202@users.noreply.github.com>
2024-11-24 17:53:39 +0530

    Initial UI Done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Css/Body.css";
-import PokemonCard from './PokemonCard';
-import {dummyJSON} from "./DummyJSON";
+import PokemonCard from "./PokemonCard";
+import { useState, useEffect } from "react";
 
 /**
  *
@@ -12,34 +12,91 @@ import {dummyJSON} from "./DummyJSON";
  * * * Pokemon Card 1
  * * * Pokemon Card 1
  */
-const getDropDownBar = () => {
-  return (
-    <select name="cars" id="cars">
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="mercedes">Mercedes</option>
-      <option value="audi">Audi</option>
-    </select>
-  );
-};
-
-const iterateme = () => {
-  const a = [
-    1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
-  const b = a.map((item) => <PokemonCard key ={item} pokemonData={dummyJSON} />);
-  return b;
-}
-
-
 const Body = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const fetchUserDetails = async () => {
+    try {
+      const urlsResponse = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      if (!urlsResponse.ok) {
+        throw new Error("Failed to fetch URLs");
+      }
+      const urls = await urlsResponse.json();
+
+      const fetchPromises = urls.results.map(({ url }) =>
+        fetch(url).then((response) => {
+          if (!response.ok) {
+            throw new Error(`Failed to fetch data from ${url}`);
+          }
+          return response.json();
+        })
+      );
+
+      const results = await Promise.all(fetchPromises);
+      console.log("results: ", results);
+
+
+      console.log("Results from all URLs:", results);
+      return results;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    const callFetchUserDetails = async () => {
+      const results = await fetchUserDetails();
+      setPokemonList(results);
+      console.log("Fetched results:", results);
+    };
+
+    callFetchUserDetails();
+  }, []);
+
+  const getDropDownBar1 = () => {
+    return <div className="visible-pokemon">All Pokemons</div>;
+  };
+
+  const getDropDownBar = () => {
+    return (
+      <select name="cars" id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+    );
+  };
+  const onFavouriteButtonClicked = () => {
+    const favouritePokemonList = pokemonList.slice(0, 2);
+    setPokemonList(favouritePokemonList);
+  };
+
+  const getFavouritesButton = () => {
+    return (
+      <button className="favourite-btn" onClick={onFavouriteButtonClicked}>
+        {" "}
+        Favourite Pokemons{" "}
+      </button>
+    );
+  };
+
+  const getPokeMons = () => {
+    return pokemonList.map((item) => (
+      <PokemonCard key={item.name} pokemonData={item} />
+    ));
+  };
+  console.log(pokemonList);
   return (
     <div className="body-container">
-      <div className="DropDown-Bar">{getDropDownBar()}</div>
+      <div className="DropDown-Bar">
+        {getDropDownBar()}
+        {getDropDownBar1()}
+
+        {getFavouritesButton()}
+      </div>
       <div className="pokemon-list-container">
-        <div className="pokemon-cars">
-          {iterateme()}
-        </div>
+        <div className="pokemon-cars">{getPokeMons()}</div>
       </div>
     </div>
   );
--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,4 +1,5 @@
-import '../Css/PokemonCard.css'
+import "../Css/PokemonCard.css";
+import { useEffect } from "react";
 
 const PokemonCard = (props) => {
   const { pokemonData } = props;
@@ -6,21 +7,24 @@ const PokemonCard = (props) => {
   const ability = abilities.map((abilityObj) => abilityObj.ability.name);
   const url = sprites?.other?.showdown?.front_default;
   return (
-    <div className="pokemon-card">
-      <img
-        className="pokemon-card-pic"
-        style={{ height: 100, width: 100 }}
-        alt={name}
-        src={url}
-      />
-      <div>
-        <div>{name}</div>
-        <div>{ability.join(',')}</div>
-        <div>{weight} grms</div>
-        <div>{height} cms</div>
+    <div className="pokemon-card-container">
+      <div className="pokemon-card">
+        <img
+          className="pokemon-card-pic"
+          style={{ height: 130, width: 130 }}
+          alt={name}
+          src={url}
+        />
+        <div className="pokemon-details">
+          <div>{name}</div>
+          <div>{ability.join(",")}</div>
+          <div>{weight} grms</div>
+          <div>{height} cms</div>
+        </div>
       </div>
+      <div className="add-fav-btn">Add to Favourites</div>
     </div>
   );
 };
 
-export default PokemonCard;;
+export default PokemonCard;
